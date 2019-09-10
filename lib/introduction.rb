f1a8ff70239cb5ee6748ca26feb4a389a08571d0
Shamel Lakin <christlife4u@gmail.com>
2019-09-10 15:53:28 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+
+def introduction(name)
+  puts "Hi I'm #{name} and I am super awesome !"
+end
+
+introduction("Shamel")
+
+def introduction_with_language(name, language)
+  puts "Hi I'm #{name} and I am super awesome at #{language} !" 
+end
+
+introduction_with_language("Shamel", "Ruby")
