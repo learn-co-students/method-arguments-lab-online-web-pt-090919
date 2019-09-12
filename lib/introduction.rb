87212a613a32f9c7bb044e8953c994ae10610918
Samuel Owusu <kwadwoamo10@gmail.com>
2019-09-12 20:08:23 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,12 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
+<<<<<<< HEAD
 def introdution_with_language(Dan,Ember.js)
   puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js}."
   end 
+=======
+def introdution_with_language (name,language)
+  puts "Hi, my name is #{Josh} and I am learning to program in #{language}."
+end 
+>>>>>>> daa05ff8205c5f54ec8643c56b3bfc679d912300
